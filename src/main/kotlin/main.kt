2b7eb5a48a2803b4fbc1855694e31e86083c2ebf
Natalya Podorozhnaya <dragonam1986@gmail.com>
2021-07-25 20:23:49 +0300

    modified main.kt: added secondary functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,8 @@
-fun main() {
-
-    val result = agoToText(6500)
-    println("Был(а) в сети $result")
-}
-
 fun agoToText(secondsCount: Int): String {
     return when {
         (0 <= secondsCount && secondsCount <= 60) -> "только что"
-        (60 <= secondsCount && secondsCount <= 60*60) -> "x минут назад" //вместо х вызов дополнительной функции
-        (60 * 60 + 1 <= secondsCount && secondsCount <= 24 * 60 * 60) -> "x часов назад" //вместо х вызов дополнительной функции
+        (60 <= secondsCount && secondsCount <= 60*60) -> "${secondToMinutesToText(secondsCount)} назад" //вместо х вызов дополнительной функции minute
+        (60 * 60 + 1 <= secondsCount && secondsCount <= 24 * 60 * 60) -> "${secondToHoursToText(secondsCount)} назад" //вместо х вызов дополнительной функции
         (24 * 60 * 60 + 1 <= secondsCount && secondsCount <= 24 * 60 * 60 * 2) -> "сегодня"
         (24 * 60 * 60 * 2 + 1 <= secondsCount && secondsCount <= 24 * 60 * 60 * 3) -> "вчера"
         else ->  {
@@ -17,3 +11,30 @@ fun agoToText(secondsCount: Int): String {
     }
 }
 
+fun secondToMinutesToText(secondsCount: Int): String {
+    val minutesCount = secondsCount/60
+    return when {
+        (minutesCount % 10 == 1 && minutesCount !== 11) -> "$minutesCount минуту"
+        (minutesCount % 10 == 2 || minutesCount % 10 == 3 || minutesCount % 10 == 4 ) -> "$minutesCount минуты"
+        else -> {
+            "$minutesCount минут"
+        }
+    }
+}
+
+fun secondToHoursToText(secondsCount: Int): String {
+    val hoursCount = secondsCount/(60*60)
+    return when {
+        (hoursCount % 10 == 1 && hoursCount !== 11) -> "$hoursCount минуту"
+        (hoursCount % 10 == 2 || hoursCount % 10 == 3 || hoursCount % 10 == 4 ) -> "$hoursCount минуты"
+        else -> {
+            "$hoursCount минут"
+        }
+    }
+}
+
+fun main() {
+    val secondsCount: Int = 39_600
+    val result = agoToText(secondsCount)
+    println("Был(а) в сети $result")
+}
